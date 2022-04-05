33d881a10db87054f1d608838916158be8db4404
Bertrand Steinmetz <bstenmm@gmail.com>
2022-04-05 17:47:03 +0200

    Random commit for heroku

--- src/pages/UserAssetsPage.tsx
+++ src/pages/UserAssetsPage.tsx
@@ -1,6 +1,12 @@
 // import { UserAssets } from 'features/userAssets/UserAssets';
 
+import { Centered } from 'components/Centered';
+
 export const UserAssetsPage = (): JSX.Element => {
-    return <div>UserAssets</div>;
+    return (
+        <Centered>
+            <div style={{ fontSize: '32px' }}>User Pruchases</div>
+        </Centered>
+    );
     // return <UserAssets />;
 };
