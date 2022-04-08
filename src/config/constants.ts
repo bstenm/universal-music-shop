80330fde8018faa0d141a143976bc2f4e91a4b5e
Bertrand Steinmetz <bstenmm@gmail.com>
2022-04-08 10:18:13 +0200

    Maintenance

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -8,4 +8,5 @@ export const SHOW_RENDERED = true;
 
 export const SHOPIFY_DOMAIN = 'programming-9190.myshopify.com';
 
-export const SHOPIFY_STOREFRONT_ACCESS_TOKEN = '8090124e9b9b8f3c14952e237a49de38';
+export const SHOPIFY_STOREFRONT_ACCESS_TOKEN =
+    process.env.REACT_APP_SHOPIFY_STOREFRONT_ACCESS_TOKEN;
